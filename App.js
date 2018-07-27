12e074e28fc33dad3bbdf26e56a0fe9fd10a1e31
mdurban <murban117@gmail.com>
2018-07-27 14:03:52 -0400

    now with dice rolling

--- App.js
+++ App.js
@@ -4,25 +4,37 @@ import { Platform, StatusBar, StyleSheet, Text, View, Button } from 'react-nativ
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { score: 20 }
+    this.state = { score: 20, dieRoll: '?' }
     this.onButtonPress = this.onButtonPress.bind(this)
+    this.rollDie = this.rollDie.bind(this)
   }
 
   onButtonPress(value) {
     this.setState({ score: this.state.score + value })
   }
 
+  rollDie() {
+    min = Math.ceil(1)
+    max = Math.floor(8)
+
+    this.setState({dieRoll: Math.floor(Math.random() * (max - min + 1) + min)})
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.score}>{this.state.score}</Text>
-        <View style={styles.grid}>
+        <View style={styles.row}>
           <Text style={styles.negButton} onPress={() => this.onButtonPress(-1)}>-1</Text>
           <Text style={styles.negButton} onPress={() => this.onButtonPress(-5)}>-5</Text>
         </View>
-        <View style={styles.grid}>
+        <View style={styles.row}>
           <Text style={styles.posButton} onPress={() => this.onButtonPress(1)}>1</Text>
           <Text style={styles.posButton} onPress={() => this.onButtonPress(5)}>5</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.roll} onPress={() => this.rollDie()}>Roll Die</Text>
+          <Text>{this.state.dieRoll}</Text>
         </View>
       </View>
     )
@@ -43,8 +55,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center'
   },
-  grid: {
+  row: {
     flexDirection: 'row'
+  },
+  roll: {
+    backgroundColor: 'white',
+    fontSize: 40,
+    textAlign: 'center',
+    borderWidth: 2.0,
+    height: 50
   },
   negButton: {
     flex: 1,
