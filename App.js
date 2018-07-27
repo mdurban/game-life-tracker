302a7178c7669684aaa53d32f2ab98d94a6383aa
mdurban <murban117@gmail.com>
2018-07-27 13:40:37 -0400

    updated styling to make colors obnoxious and font too big

--- App.js
+++ App.js
@@ -16,10 +16,14 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.score}>{this.state.score}</Text>
-        <Text style={styles.negButton} onPress={() => this.onButtonPress(-1)}>-1</Text>
-        <Text style={styles.negButton} onPress={() => this.onButtonPress(-5)}>-5</Text>
-        <Text style={styles.posButton} onPress={() => this.onButtonPress(1)}>1</Text>
-        <Text style={styles.posButton} onPress={() => this.onButtonPress(5)}>5</Text>
+        <View style={styles.grid}>
+          <Text style={styles.negButton} onPress={() => this.onButtonPress(-1)}>-1</Text>
+          <Text style={styles.negButton} onPress={() => this.onButtonPress(-5)}>-5</Text>
+        </View>
+        <View style={styles.grid}>
+          <Text style={styles.posButton} onPress={() => this.onButtonPress(1)}>1</Text>
+          <Text style={styles.posButton} onPress={() => this.onButtonPress(5)}>5</Text>
+        </View>
       </View>
     )
   }
@@ -29,21 +33,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
-    backgroundColor: 'cyan'
+    backgroundColor: 'cyan',
+    justifyContent: 'center',
   },
   score: {
-    flex: 2,
-    fontSize: 108,
+    flex: 1,
+    alignSelf: 'center',
+    fontSize: 200,
     fontWeight: 'bold',
     textAlign: 'center'
   },
+  grid: {
+    flexDirection: 'row'
+  },
   negButton: {
     flex: 1,
-    backgroundColor: 'red'
+    alignSelf: 'center',
+    fontSize: 82,
+    textAlign: 'center',
+    backgroundColor: 'red',
+    borderWidth: 2.0
   },
   posButton: {
     flex: 1,
-    backgroundColor: 'blue'
+    alignSelf: 'center',
+    fontSize: 82,
+    textAlign: 'center',
+    backgroundColor: 'blue',
+    borderWidth: 2.0
   }
 })
 
