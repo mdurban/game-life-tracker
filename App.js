0a57fc0e9653091ddcaa34e0ab9c89edce5b8094
mdurban <murban117@gmail.com>
2018-07-31 15:27:53 -0400

    add background and change color scheme to match img

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Platform, StatusBar, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StatusBar, StyleSheet, ImageBackground, Text, View, Button } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props)
+
     this.state = { score: 20, dieRoll: '?' }
     this.onButtonPress = this.onButtonPress.bind(this)
     this.rollDie = this.rollDie.bind(this)
   }
-
+  
   onButtonPress(value) {
     this.setState({ score: this.state.score + value })
   }
@@ -23,7 +24,9 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.score}>{this.state.score}</Text>
+        <ImageBackground source={require('./cradle.jpg')} style={styles.imageBackground}>
+          <Text style={styles.score}>{this.state.score}</Text>
+        </ImageBackground>
         <View style={styles.row}>
           <Text style={styles.negButton} onPress={() => this.onButtonPress(-1)}>-1</Text>
           <Text style={styles.negButton} onPress={() => this.onButtonPress(-5)}>-5</Text>
@@ -33,53 +36,69 @@ export default class App extends Component {
           <Text style={styles.posButton} onPress={() => this.onButtonPress(5)}>5</Text>
         </View>
         <View style={styles.row}>
-          <Text style={styles.roll} onPress={() => this.rollDie()}>Roll Die</Text>
-          <Text>{this.state.dieRoll}</Text>
+          <Text style={styles.roll} onPress={() => this.rollDie()}><Text>Roll d20:</Text><Text>{this.state.dieRoll}</Text></Text>
         </View>
       </View>
     )
   }
 }
 
+// red: #974B45, blue: #8CBFD3, background: #A67F43, dice: #BAA498
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
-    backgroundColor: 'cyan',
-    justifyContent: 'center',
+    backgroundColor: '#e6ffff',
+    justifyContent: 'center'
   },
   score: {
     flex: 1,
     alignSelf: 'center',
     fontSize: 200,
+    fontFamily: 'monospace',
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  dieRoll: {
+    fontSize: 50,
+    fontFamily: 'Roboto'
+  },
+  imageBackground: {
+    flex: 1,
   },
   row: {
     flexDirection: 'row'
   },
   roll: {
+    alignSelf: 'center',
     backgroundColor: 'white',
-    fontSize: 40,
+    fontSize: 30,
+    fontFamily: 'monospace',
     textAlign: 'center',
     borderWidth: 2.0,
-    height: 50
+    width: '100%',
   },
   negButton: {
     flex: 1,
     alignSelf: 'center',
     fontSize: 82,
+    fontFamily: 'monospace',
     textAlign: 'center',
-    backgroundColor: 'red',
-    borderWidth: 2.0
+    backgroundColor: '#b4615b',
+    borderWidth: 2.0,
+    borderColor: '#6D6D6E',
+    color: '#6D6D6E'
   },
   posButton: {
     flex: 1,
     alignSelf: 'center',
     fontSize: 82,
     textAlign: 'center',
-    backgroundColor: 'blue',
-    borderWidth: 2.0
+    fontFamily: 'monospace',
+    backgroundColor: '#8CBFD3',
+    borderWidth: 3.0,
+    borderColor: '#6D6D6E',
+    color: '#6D6D6E'
   }
 })
 
